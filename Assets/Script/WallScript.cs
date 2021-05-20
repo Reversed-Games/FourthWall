252db6af8af0071545a603f607569d6f2dcde897
KadotCom (Kaiden) <76606192+kadotcom@users.noreply.github.com>
2021-05-20 10:06:17 -0400

    Update WallScript.cs

--- Assets/Script/WallScript.cs
+++ Assets/Script/WallScript.cs
@@ -5,13 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class WallScript : MonoBehaviour
 {
-    // Reversed Game Studio Presents
     private void OnCollisionEnter(Collision collision)
     {
        if(SceneManager.GetActiveScene().name == "Lvl1")
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene("Lvl2");
             Debug.Log("Lvl1 - Wall touched");
+        }
+        if (SceneManager.GetActiveScene().name == "Lvl2")
+        {
+            SceneManager.LoadScene("Lvl3");
+            Debug.Log("Lvl2 - Wall touched");
+        }
+        if (SceneManager.GetActiveScene().name == "Lvl3")
+        {
+            SceneManager.LoadScene("Lvl4");
+            Debug.Log("Lvl3 - Wall touched");
+        }
+
+        if (SceneManager.GetActiveScene().name == "Lvl4")
+        {
+            SceneManager.LoadScene(0);
+            Debug.Log("Lvl4 - Wall touched");
+        }
+
+        if (SceneManager.GetActiveScene().name == "Secret")
+        {
+            SceneManager.LoadScene(0);
+            Debug.Log("Secret - Wall touched");
         }
     }
 
